adfb213245c678c37fe2e4dfa07dd1657a289b36
yamat0jp <yamat0jp@yahoo.co.jp>
2018-09-01 13:11:31 +0900

    update

--- bot.py
+++ bot.py
@@ -7,21 +7,23 @@ Created on Sat Sep  1 11:18:39 2018
 
 import tornado.ioloop
 import tornado.web
-import json
+import json, os
 
 
-verify_token = <VERIFY_TOKEN>
-
-class WebhookHandler(tornado.web.RequestHandler):
+class WebHookHandler(tornado.web.RequestHandler):
     def get(self):
         token = self.get_argument('hub.verify_token','')
-        if token == verify_token:
+        if token == ch:
             write(token)
         else:
-            write('Error, wrong varidation Token.)
+            write('Error, wrong varidation Token.')
 
-application = tornado.web.Application([(r'webhook',WebHookHandler)])
+application = tornado.web.Application([(r'/callback',WebHookHandler)],{
+        'debug':True
+        })
 
 if __name__ == '__main__':
+    ch_ic = os.environ['Channel_ID']
+    ch = os.environ['Channel_Secret']
     application.listen(5000)
     tornado.ioloop.IOLoop.instance().start()
