ba5b93553e6161fe1d400f0bdfd2debac620af21
yamat0jp <yamat0jp@yahoo.co.jp>
2018-09-01 14:49:37 +0900

    library install

--- bot.py
+++ bot.py
@@ -8,22 +8,39 @@ Created on Sat Sep  1 11:18:39 2018
 import tornado.ioloop
 import tornado.web
 import json, os
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 
 class WebHookHandler(tornado.web.RequestHandler):
-    def get(self):
-        token = self.get_argument('hub.verify_token','')
-        if token == ch:
-            write(token)
-        else:
-            write('Error, wrong varidation Token.')
+    def post(self):
+        global webhook
+        signature = json.load(self.request.headers['X-Line-Signature'])
+        data = json.load(self.request.body)
+        try:
+            events = webhook.parse(data, signature)
+        except InvalidSignatureError:
+            abort(400)
+        for event in events:
+            if not isinstance(event,MessageEvent):
+                continue
+            if not isinstance(event.message,TextMessage):
+                continue
+            linebot.reply_message(
+                event.rply_token,
+                TextSendMessage(text=event.message.text)
+            )
+        return 'OK'
 
 application = tornado.web.Application([(r'/callback',WebHookHandler)],{
         'debug':True
         })
 
 if __name__ == '__main__':
-    ch_ic = os.environ['Channel_ID']
+    ch_id = os.environ['Channel_ID']
     ch = os.environ['Channel_Secret']
+    linebot = LineBotApi(ch_id)
+    Webhook = WebhookParser(ch)  
     application.listen(5000)
     tornado.ioloop.IOLoop.instance().start()
