c88b55b6b9bc8fa08f20959545c8580db60f4c36
yamat0jp <yamat0jp@yahoo.co.jp>
2018-09-04 20:44:09 +0900

    linebot フォルダー復活
    必要不可欠だったようです

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ Created on Sat Sep  1 11:18:39 2018
 import tornado.ioloop
 import tornado.web
 import tornado.escape
-import json, os, hmac, base64, hashlib, pytz, pymongo
+import json, os, hmac, base64, hashlib, pytz, pymongo, re
 from datetime import datetime
 from linebot import LineBotApi, WebhookParser, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -29,13 +29,26 @@ class WebHookHandler(tornado.web.RequestHandler):
         db = pymongo.MongoClient(uri)[ac]
         table = db['glove']
         if table.find().count() == 0: 
-            table.insert_one({u'リフレフィット':'SF'})
-        self.write(str(t))
+            table.insert_one({'name':u'リフレフィット','no':'SF'})
+        no = 'SF'
+        item = table.find({'no':no})
+        if item.count() == 1:
+            dic = {item['name']:item['no']}
+        elif item.count() == 0:
+            dic = {}
+            for x in list(item.sort('no')):
+                dic[x['name']] = x['no']
+        else:
+            item = table.find({'no':re.compile(no)})
+            dic = {}
+            for x in list(item.sort('no')):
+                dic[x.name] = x['no']
+        self.write(dic)
         
     def post(self):
         header = json.load(self.request.headers)
         body = json.load(self.request.body)
-        hash = hmac.new(header['X-Line-Signature'].encode('utf-8'),
+        hash = hmac.new(header['X-LINE-SIGNATURE'].encode('utf-8'),
             body.encode('utf-8'), hashlib.sha256).digest()
         signature = base64.b64encode(hash)
         '''
